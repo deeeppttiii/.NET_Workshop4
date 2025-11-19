0b96d3e77dca65d644f8f4b3f3d6e3297451611f
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-11-19 21:33:21 +0545

    Added Task 5 Code

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Task_5
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Enter a day (Example: Sunday): ");
+        string day = Console.ReadLine().Trim().ToLower();
+
+        DayType type;
+
+        if (day == "friday" || day == "saturday")
         {
-            Console.WriteLine("Hello, World!");
+            type = DayType.Weekend;
         }
+        else
+        {
+            type = DayType.Weekday;
+        }
+
+        Console.WriteLine($"It is: {type}");
+
+
+        Book book1 = new Book("Atomic Habits", "James Clear", 999.50);
+
+        Book book2 = book1 with { title = "The Alchemist", price = 850.00 };
+
+        Console.WriteLine("\nBook 1 Details:");
+        Console.WriteLine(book1);
+
+        var (title, author, price) = book2;
+
+        Console.WriteLine("\nBook 2 Deconstructed Values:");
+        Console.WriteLine($"Title = {title}");
+        Console.WriteLine($"Author = {author}");
+        Console.WriteLine($"Price = {price}");
     }
 }
