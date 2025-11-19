67a6c15951197bf28753a4f96c49b4d791e048b9
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-11-19 12:15:10 +0545

    Added Task 4 Code

---
+++ Task 4/Player.cs
@@ -0,0 +1,18 @@
+﻿public class Player
+{
+    public string playerName;
+    public int level;
+    public int health;
+
+    public Player()
+    {
+        Console.WriteLine("Default constructor has been called");
+    }
+
+    public Player(string name, int level, int health)
+    {
+        this.playerName = name;
+        this.level = level;
+        this.health = health;
+    }
+}
--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Task_4
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Player p1 = new Player();
+
+        Console.WriteLine("\nPlayer 1 Details:");
+        Console.WriteLine("Name: " + p1.playerName);
+        Console.WriteLine("Level: " + p1.level);
+        Console.WriteLine("Health: " + p1.health);
+
+        Player p2 = new Player("Alex", 5, 100);
+
+        Console.WriteLine("\nPlayer 2 Details:");
+        Console.WriteLine("Name: " + p2.playerName);
+        Console.WriteLine("Level: " + p2.level);
+        Console.WriteLine("Health: " + p2.health);
     }
 }
