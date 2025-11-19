7b4ded9dd948dbdb27ac1fd580e52d6d01c98bf6
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-11-19 12:02:49 +0545

    Added Task 2 Code

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Task_2
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Calculator calc = new Calculator();
+
+        calc.PrintWelcome();
+
+        int sum = calc.Add(5, 7);
+        int product1 = calc.Multiply(4, 3); 
+        int product2 = calc.Multiply(6);     
+
+        Console.WriteLine("Addition: " + sum);
+        Console.WriteLine("Multiplication (4 * 3): " + product1);
+        Console.WriteLine("Multiplication with default value (6 * 1): " + product2);
     }
 }
