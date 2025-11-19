5232da4c585607f3c960850dc699efd36b8b02ee
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-11-19 11:53:04 +0545

    Added Task 1 Code

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Task_1
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        s1.name = "John";
+        s1.age = 20;
+        s1.address = "Kathmandu";
+
+        s2.name = "Deepti";
+        s2.age = 21;
+        s2.address = "Morang";
+
+        Console.WriteLine("Student 1 Details:");
+        Console.WriteLine("Name: " + s1.name);
+        Console.WriteLine("Age: " + s1.age);
+        Console.WriteLine("Address: " + s1.address);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Student 2 Details:");
+        Console.WriteLine("Name: " + s2.name);
+        Console.WriteLine("Age: " + s2.age);
+        Console.WriteLine("Address: " + s2.address);
+
+        Console.WriteLine();
+
+        Console.WriteLine("School Name (Static Field): " + Student.schoolName);
     }
 }
---
+++ Task 1/Student.cs
@@ -0,0 +1,8 @@
+﻿public class Student
+{
+    public string name;
+    public int age;
+    public string address;
+
+    public static string schoolName = "Itahari International School";
+}
