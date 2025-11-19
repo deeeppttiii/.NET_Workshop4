2293fd69e3602f3c0b63b199f747ab4b26682ff3
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-11-19 12:09:23 +0545

    Added Task 3 Code

---
+++ Task 3/Parameter.cs
@@ -0,0 +1,22 @@
+﻿public class Parameter
+{
+    public void Increase(ref int number)
+    {
+        number += 10;
+    }
+
+    public void GetFullName(out string fullname)
+    {
+        fullname = "Deepti Dhungana";  
+    }
+
+    public int SumAll(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        return sum;
+    }
+}
--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Task_3
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Parameter demo = new Parameter();
+
+        int value = 20;
+        demo.Increase(ref value);
+        Console.WriteLine("After Increase: " + value);
+
+        demo.GetFullName(out string myName);
+        Console.WriteLine("Full Name: " + myName);
+
+        int result = demo.SumAll(5, 10, 15, 20);
+        Console.WriteLine("Sum of all numbers: " + result);
     }
 }
